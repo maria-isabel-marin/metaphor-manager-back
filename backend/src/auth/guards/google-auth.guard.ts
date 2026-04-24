6fd48e45577a084bbe3e41e1476517e587945fe0
JhonUsuga <jhon.usuga1@udea.edu.co>
2026-04-23 22:05:14 -0500

    Se ajustó el guard de autenticación de Google para forzar la selección de cuenta en cada nuevo inicio de sesión.

--- backend/src/auth/guards/google-auth.guard.ts
+++ backend/src/auth/guards/google-auth.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+// se cambia todo el auth para que pueda cerrar la sesion
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class GoogleAuthGuard extends AuthGuard('google') {}
+export class GoogleAuthGuard extends AuthGuard('google') {
+  getAuthenticateOptions(_context: ExecutionContext) {
+    return {
+      prompt: 'select_account',
+    };
+  }
+}
